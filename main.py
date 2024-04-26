972789546067fe20717315f767d2d0fdb90e730c
boomsi <libo19431@gmail.com>
2024-04-26 16:07:56 +0800

    other

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from typing import Union
+from enum import Enum
 
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
+
+class ModelName(str, Enum):
+    alexnet = 'alexnet',
+    resnet = 'resnet',
+    lenet = 'lenet'
 
 class Item(BaseModel):
     name: str
@@ -27,3 +33,18 @@ def update_item(item_id: int, item: Item):
         item_id=item_id,
         item_name=item.name
     )
+
+@app.get('/models/{model_name}')
+def get_module(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return dict(model_name=model_name, message='Deep Learning FTW!')
+    elif model_name is ModelName.lenet:
+        return dict(model_name=model_name, message='LeCNN all the images')
+    elif model_name is ModelName.resnet:
+        return dict(model_name=model_name, message='Have some residuals')
+
+
+@app.get('/items')
+async def get_items(page: int = 0, limit: int = 10):
+    return dict(page=page, limit=limit)
+
