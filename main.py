6406b33750c40126d4b9edd4908dd6733f249169
boomsi <libo19431@gmail.com>
2024-04-28 20:04:10 +0800

    other test

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from typing import Union
+from typing import Annotated, Dict
 from enum import Enum
 
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, Query, Path, Body, Cookie, Header, Depends
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
@@ -11,17 +11,30 @@ class ModelName(str, Enum):
     resnet = 'resnet',
     lenet = 'lenet'
 
+    model_config = dict(
+        json_schema_extra=dict(
+            examples=[
+                dict(
+                    alexnet='Deep Learning FTW!',
+                    resnet='Have some residuals',
+                    lenet='LeCNN all'
+                )
+            ]
+        )
+    )
+
 class Item(BaseModel):
     name: str
-    price: float
-    is_offer: Union[bool, None] = None
+    description: str | None = None
+    price: float = Field(gt=10)
+    tax: float | None = None
 
 @app.get('/')
 def read_root():
     return dict(a=1)
 
 @app.get('/item/{item_id}')
-def read_item(item_id:int, q: Union[str, None] = None):
+def read_item(item_id: Annotated[int, Path(gt=1)], q: str | None = None):
     return dict(
         item_id=item_id,
         q=q
@@ -44,7 +57,40 @@ def get_module(model_name: ModelName):
         return dict(model_name=model_name, message='Have some residuals')
 
 
+# @app.get('/items')
+# async def get_items(page: int = 0, limit: int = 10):
+#     return dict(page=page, limit=limit)
+
+
 @app.get('/items')
-async def get_items(page: int = 0, limit: int = 10):
+def get_items(page: Annotated[int, Query(gt=100)] = 101, limit: int = Query(max=50, deprecated=True)):
     return dict(page=page, limit=limit)
 
+@app.post('/abc')
+def get_abc(item: Item, importance: Annotated[int, Body(gt=0)]):
+    return dict(importance=importance)
+
+
+@app.get('/cookie')
+def get_cookie(ad: Annotated[str, Cookie()]):
+    return dict(ad=ad)
+
+@app.get('/header', response_model=Item, response_model_include=['price'], response_model_exclude_unset=True)
+def get_header(token: Annotated[str, Header()]) -> Item:
+    return Item(name=token, price=10.5)
+
+
+# def get_depend_common(q: Dict[str, int]):
+#     new_q = dict()
+#     for k, v in q.items():
+#         new_q[k] = v + 1
+#     return new_q
+
+class CommonDepends:
+    def __init__(self, common: str, q: int):
+        self.common = common
+        self.q = q + 10086
+
+@app.get('/depends')
+def get_depend(commons: Annotated[CommonDepends, Depends(CommonDepends)]):
+    return commons
